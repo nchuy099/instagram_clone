8581edd056189b43c2ddc411b70f7df419c79ab9
nchuy099 <nchihuy099@gmail.com>
2026-01-16 14:55:21 +0700

    build: init project base

---
+++ src/main/java/com/nchuy099/mini_instagram/MiniInstagramApplication.java
@@ -0,0 +1,13 @@
+package com.nchuy099.mini_instagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniInstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniInstagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nchuy099/mini_instagram/MiniInstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nchuy099.mini_instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniInstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
