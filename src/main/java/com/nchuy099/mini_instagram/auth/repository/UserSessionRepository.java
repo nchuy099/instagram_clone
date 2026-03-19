6541d5d1da5c4e973ac9e319428334eed701397a
nchuy099 <nchihuy099@gmail.com>
2026-03-19 15:14:52 +0700

    feat(backend): implement User entity, repository, and JWT security setup

---
+++ src/main/java/com/nchuy099/mini_instagram/auth/repository/UserSessionRepository.java
@@ -0,0 +1,12 @@
+package com.nchuy099.mini_instagram.auth.repository;
+
+import com.nchuy099.mini_instagram.auth.entity.UserSession;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserSessionRepository extends JpaRepository<UserSession, Long> {
+    Optional<UserSession> findByRefreshTokenHash(String refreshTokenHash);
+}
